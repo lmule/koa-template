c8ecd7a2be8cdef7933aeb8b3f86bd9fee3c14ff
丁锐 <dingrui@daojia.com>
2018-03-22 14:13:25 +0800

    break;

--- app/middleware/router.js
+++ app/middleware/router.js
@@ -3,6 +3,9 @@ const routerPath = require("path").join(__dirname, "../router");
 // 遍历router下的所有路由文件，这样就不用再加router/index.js来包含所有文件了
 module.exports = app => {
     require("fs").readdirSync(routerPath).forEach(function(file) {
+        if (!file.endsWith('.js')) {
+            return;
+        }
         const router = require("../router/" + file).routes();
         app.use(router)
     });
