660830f68cfb605f3fd72942fcb2abb163758fab
dingrui <dingrui@daojia.com>
2018-06-19 11:30:23 +0800

    add axios post params

--- app/helper/request.js
+++ app/helper/request.js
@@ -4,14 +4,14 @@ const log = require('./log')
 module.exports = {
     get: async url => {
         const start = (new Date()).getTime()
-        const {data} = await axios.get(url)
+        const { data } = await axios.get(url)
         const duration = (new Date()).getTime() - start
         log.info('GET请求第三方url:%s 共耗时%s毫秒', url, duration)
         return data
     },
-    post: async url => {
+    post: async (url, param) => {
         const start = (new Date()).getTime()
-        const {data} = await axios.post(url)
+        const { data } = await axios.post(url, params)
         const duration = (new Date()).getTime() - start
         log.info('POST请求第三方url:%s 共耗时%s毫秒', url, duration)
         return data
