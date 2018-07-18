6f664f734a8231829608c6f9168b1c9fe3837977
dingrui <dingrui@daojia.com>
2018-07-18 15:05:41 +0800

    将配置项支持多环境

--- config/param.js
+++ config/param.js
@@ -1,3 +1,20 @@
-module.exports = {
+const config = require('../app/helper/config')
+
+let params = {
     "defaultPort": "<%- port%>"
 }
+
+let envParams = {
+    'development': {
+        'serverHost': 'http://localhost',
+    },
+    'test': {
+        'serverHost': 'http://localhost',
+    },
+    'production': {
+        'serverHost': 'http://example.com',
+    }
+}
+Object.assign(params, config(envParams))
+
+module.exports = params
